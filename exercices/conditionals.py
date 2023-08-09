6d78ff740290a5c0fce0a5415a2f30ee251a3da4
adriana <adrianafs1991@gmail.com>
2023-08-09 18:16:08 -0500

    conditionals

---
+++ exercices/conditionals.py
@@ -0,0 +1,11 @@
+# ejemplo 1
+print('####### EJERCICIO 1 ########')
+
+color = input('adivina cual es el color de la encuesta ')
+
+if color != 'blue':
+    print(f"El color es incorrecto")
+else :
+    print(f"Correcto el color es: {color}")
+    
+    
