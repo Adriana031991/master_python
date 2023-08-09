2265effed18fe6f4636c3f1dcc1ef5b901f6be8e
adriana <adrianafs1991@gmail.com>
2023-08-09 18:26:40 -0500

    conditionals with comparitions

--- exercices/conditionals.py
+++ exercices/conditionals.py
@@ -1,5 +1,5 @@
 # ejemplo 1
-print('####### EJERCICIO 1 ########')
+print('\n####### EJERCICIO 1 ########\n')
 
 color = input('adivina cual es el color de la encuesta ')
 
@@ -9,3 +9,11 @@ else :
     print(f"Correcto el color es: {color}")
     
     
+print('\n####### EJERCICIO 2 ########\n')
+
+year = int(input('En que año estamos '))
+
+if year >= 2021:
+    print(f"\nEl año es mayor a 2021")
+else:
+    print(f"\nEl año es menor a 2021")
