7786c0071ac178bb3523482edfafeb93b13472a6
adriana <adrianafs1991@gmail.com>
2023-08-09 18:40:09 -0500

    nested conditionals

--- exercices/conditionals.py
+++ exercices/conditionals.py
@@ -17,3 +17,24 @@ if year >= 2021:
     print(f"\nEl año es mayor a 2021")
 else:
     print(f"\nEl año es menor a 2021")
+
+print('\n####### EJERCICIO 3 ########\n')
+
+name = input('escriba un nombre ')
+edad = int(input('escriba una edad '))
+residencia = input('escriba el pais de residencia ')
+mayoria_edad = 18
+
+if residencia != 'Colombia':
+     print(f"{name} no es colombiano")
+
+else:
+    print(f"{name} es colombiano")
+    
+    if edad >= mayoria_edad:
+        print(f"{name} es mayor de edad")
+        
+    else:
+        print(f"{name} No es mayor de edad")
+      
+
