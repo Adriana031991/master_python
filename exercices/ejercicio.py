a17a0286356925f255d1b3e0d42e13d472a82d00
adriana <adrianafs1991@gmail.com>
2023-08-10 16:55:19 -0500

    first excercises

---
+++ exercices/ejercicio.py
@@ -0,0 +1,39 @@
+# print('######### primero ######')
+
+# pais = 'colombia'.capitalize() #str
+# continente = 'america'.capitalize() #str
+
+# print(f' el pais {pais} está en el continente {continente}')
+
+# print('######### segundo ######')
+# imprimir numeros pares
+
+# opt1
+# for x in range(0, 121, 2):
+#  print(x)
+
+# opt2
+# contador = 1
+# while contador <=120:
+#     res = contador % 2 == 0
+#     if res :
+#         print(contador)
+#     contador += 1
+    
+    
+# print('######### tercero ######')
+
+# imprimir los cuadrados de los primeros 60 numeros
+
+contador = 1
+while contador <= 60:
+    a = contador*contador
+    print(a)
+    contador += 1
+    
+for item in range(0,61):
+    b = item*item
+    print(b)
+    
+ 
+# print('######### cuarto ######')
