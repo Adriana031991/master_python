0183fc3814ef884683e42d7b4fda35240e333778
adriana franklin sanchez <adrianafs1991@gmail.com>
2023-08-14 16:32:48 -0500

    add four exercice

--- exercices/ejercicio.py
+++ exercices/ejercicio.py
@@ -19,21 +19,42 @@
 #     if res :
 #         print(contador)
 #     contador += 1
-    
-    
+
+
 # print('######### tercero ######')
 
 # imprimir los cuadrados de los primeros 60 numeros
 
-contador = 1
-while contador <= 60:
-    a = contador*contador
-    print(a)
-    contador += 1
-    
-for item in range(0,61):
-    b = item*item
-    print(b)
-    
- 
+# contador = 1
+# while contador <= 60:
+#     a = contador*contador
+#     print(a)
+#     contador += 1
+
+# for item in range(0,61):
+#     b = item*item
+#     print(b)
+
+
 # print('######### cuarto ######')
+
+# pedir dos numeros al usuario y hacer todas las operaciones matematicas
+
+# num = int(input("\ningrese el primer numero: "))
+# num1 = int(input("ingrese el segundo numero: "))
+
+
+# suma = num + num1
+# resta = num - num1
+# multi = num * num1
+# div = num / num1
+# mod = num % num1
+# print("\n·········· Calculadora···················\n")
+# print(f"la suma entre {num} y {num1} es: {suma}")
+# print(f"la resta entre {num} y {num1} es: {resta}")
+# print(f"la multiplicacion entre {num} y {num1} es: {multi}")
+# print(f"la division entre {num} y {num1} es: {div}")
+# print(f"la modulo entre {num} y {num1} es: {mod}")
+
+
+# print('######### Quinto ######')
